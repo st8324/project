649dd57e3478138dc195e454c821a8754b6cb5b3
stajun <stajun@naver.com>
2019-11-14 15:34:51 +0900

    27일차] 수업 내용 - 네이버 만들기2

---
+++ study/day26/js/naver예제.js
@@ -0,0 +1,13 @@
+$(function(){
+	$('.keyboard').click(function(){
+		$('.keyboard-icon').toggleClass('active');
+	})
+	$('.auto').click(function(){
+		$('.auto-icon').toggleClass('active');
+		$('.auto-box').toggle();
+	})
+	$('.more').click(function(){
+		$(this).toggleClass('fold');
+		$('.more-box').toggle();
+	})
+})

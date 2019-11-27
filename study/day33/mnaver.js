b3c6522873ce983d612eac865b5180f85c4ad7ea
stajun <stajun@naver.com>
2019-11-27 15:59:43 +0900

    36일차] 수업 내용 - 모바일 네이버 - 메뉴 구현

--- study/day33/mnaver.js
+++ study/day33/mnaver.js
@@ -26,4 +26,29 @@ $(function(){
 		$('.search-list-box').addClass('display-none');
 		$('.body').removeClass('display-none');
 	})
+
+	$('.card-item').click(function(){
+		$(this).toggleClass('card-item-select');
+		$(this).find('.card-subs')
+					 .toggleClass('card-subs-select');
+		//선택된 언론사 갯수
+		var count = $('.card-subs-select').length;
+		if(count == 0){
+			$('.subs-count-box').addClass('display-none');
+		}else{
+			var str = count + '개 언론사 구독';
+			$('.subs-count-box').text(str);
+			$('.subs-count-box').removeClass('display-none');
+		}
+	})
+
+	$('.menu-item').click(function(){
+		if($(this).hasClass('sel-pink')){
+			$('.menu-list').addClass('menu-list2');
+			$('.menu-plus').addClass('display-none');
+		}else{
+			$('.menu-list').removeClass('menu-list2');
+			$('.menu-plus').removeClass('display-none');
+		}
+	})
 })
